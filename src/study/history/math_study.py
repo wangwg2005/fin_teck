3ce8c9b4c84b452e9220043e8bcbfe91d02bfec7
wangwg <wangwg2005@163.com>
2021-01-04 22:35:35 +0800

    study history

--- src/study/history/math_study.py
+++ src/study/history/math_study.py
@@ -1,10 +1,33 @@
 # -*- coding: utf-8 -*-
 
+import matplotlib.pyplot as plt
 import pandas as pd
 
-csi500=pd.read_csv("000905.csv",encoding="gbk")
+csi500 = pd.read_csv("000905.csv", encoding="gbk")
 
-csi500=csi500[:60]
-csi500.reindex(index=csi500.index[::-1])
+csi500=csi500[:800]
+csi500 = csi500.reindex(index=csi500.index[::-1])
+# csi500.index=range(len(csi500))
+csi500["no"]=range(len(csi500))
+date_time = pd.to_datetime(csi500.pop('日期'), format='%Y-%m-%d')
+csi500.index=date_time
 
-print(csi500[["日期","收盘价"]])
+print(csi500[["收盘价","no"]])
+
+reflect = 6200
+
+import math
+
+delta = len(csi500)-43
+
+def mode_1(b):
+    a = b - delta
+    return 650*math.cos(a/60-0.29)+5750+80*math.cos(a/5 *math.pi)
+
+def mode_2(a):
+    return 1125*math.cos(a/160+math.pi/2-0.1)+5600+80*math.cos(a/5 *math.pi)
+
+features = csi500[["收盘价"]]
+features["sim"] = list(map(mode_2, range(len(csi500))))
+features.plot(grid=True)
+plt.show()
