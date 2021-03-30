28a641aaac0e5de4173f37811faa23c3520ec84a
Darren <wangwg2005@163.com>
2021-03-30 17:10:01 +0800

    restructure model2 trainer

--- src/study/history/model_trainer2.py
+++ src/study/history/model_trainer2.py
@@ -4,63 +4,69 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 import model_util as mu
+import os
 
 from sklearn.linear_model import LinearRegression
+from importlib_metadata import files
 
 trade_days=1000
 
 def read_history(fname):
-    df=pd.read_csv(fname+"_latest.csv",encoding="gbk",parse_dates=[0],index_col=0)
+    df=pd.read_csv(fname,encoding="gbk",parse_dates=[0],index_col=0)
     return df
 
 def read_leverage(fname):
-    df = pd.read_excel("融资融券"+fname+"_all_latest.xls",header=1, encoding="gbk",parse_dates=[0],index_col=0)
+    df = pd.read_excel(fname,header=1, encoding="gbk",parse_dates=[0],index_col=0)
     return df
 
 def get_sub_set(df,start_date, end_date):
     return df[end_date:start_date]
+
+### price file first , leverage data second
+def read_dir(path):
+    files = os.listdir( path );
+    
+    if files[0][-3:]=="csv":
+        return files
+    else :
+        return (files[1],files[0])
     
 
-def model1_train():
-    csi500=read_history("000905")
+def model1_train(train_start="2019-01-01", train_end="2020-12-31", his_start="2017-01-01", his_end=""):
+    
 #     features=csi500[["最高价","最低价","开盘价","收盘价"]]
 #     print(features)
+#     root="csi500"
+    names=["csi500","hs300","399006","000016"]
+    root=names[3]
+    price_file, leverage_file=read_dir(root)
     
-    leve_csi500=read_leverage("中证500")
+    csi500=read_history(os.path.join(root,price_file))
+    f1=os.path.join(root,leverage_file)
+    print(f1)
+    leve_csi500=read_leverage(f1)
     
-    features=csi500[["收盘价"]]
+    features=csi500[["收盘价","成交量"]]
+    print(csi500.index.duplicated())
     features["lev"]=leve_csi500["融资余额(亿元)"]
     features.dropna(inplace=True)
     
-    train_date=("2020-01-01","2020-12-31")
+    train_date=("2019-01-01","2020-12-31")
     train_set=get_sub_set(features, *train_date)
 #     print(train_set)
-    
-    test_date=("2021-01-01","2021-03-15")
-    test_set=get_sub_set(features, *test_date)
     
     lr=LinearRegression()
 #     X=np.array(train_set["lev"]).reshape(1, -1)
 #     y=np.array(train_set["收盘价"]).reshape(1, -1)
     lr.fit(train_set[["lev"]], train_set["收盘价"])
     
-    print(train_set["lev"].shape)
-    pred=lr.predict(train_set[["lev"]])
-    print(features.shape)
-    print(len(pred))
-    print(pred)
-#     train_set["pred"]=pred
-#     train_set["resid"]=train_set["收盘价"]-train_set["pred"]+4000
-    
-#     train_set.plot(grid=True)
-#     plt.show()
-    
     features["pred"]=lr.predict(features[["lev"]])
     features["resid"]=features["收盘价"]-features["pred"]
     features["resid_norm"]=features["resid"]/features["收盘价"]
-    features[:"2020-01-01"].plot(grid=True)
-    plt.show()
-    features[:"2020-01-01"][["resid","resid_norm"]].plot(grid=True,subplots=True)
+#     features[:"2020-01-01"].plot(grid=True)
+#     plt.show()
+    features['成交量'] = features['成交量'].astype('float64')
+    features[:his_start][["收盘价","resid","resid_norm"]].plot(grid=True,subplots=True,title=root)
     
     plt.show()
 #     test_set["pred"]=lr.predict(np.array(test_set["lev"]).reshape(1, -1))
