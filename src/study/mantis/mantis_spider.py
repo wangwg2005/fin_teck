962ac3175b1eda60d48e8d28712ac79e1904f684
Darren <wangwg2005@163.com>
2021-04-15 14:00:39 +0800

    scrap all the mantis data from sse

--- src/study/mantis/mantis_spider.py
+++ src/study/mantis/mantis_spider.py
@@ -7,9 +7,9 @@ from time import sleep
 from selenium.common.exceptions import TimeoutException
 import requests
 import matplotlib.pyplot as plt
-import file_cache as fc
+# import file_cache as fc
 import logging
-from logging.handlers import RotatingFileHandler 
+from logging.handlers import RotatingFileHandler
 import json
 from selenium.webdriver.support import expected_conditions as EC
 # from selenium.webdriver.chrome.options import Options
@@ -36,6 +36,32 @@ browser=webdriver.Chrome(options=chrome_options)
 
 def output(obj):
     logger.info(json.dumps(obj))
+    
+def click_btn2(btn,cover):
+    try:        
+        wait = WebDriverWait(browser,5);
+        wait.until(EC.invisibility_of_element(cover))        
+        wait.until(EC.element_to_be_clickable(btn ))
+    except Exception as err:
+        print("except happend when waiting error",err)
+    btn.click()
+    
+def click_btn(btn_id,cover_class):
+#     cnt=3
+#     success=False
+#     while cnt>0 and not success:
+#         try:
+#             
+#             btn.click()
+#             success=True
+#         except:
+#             time.sleep(5)
+#             cnt-=1
+#     if not success:
+#         print("btn click failed",btn)
+    btn=browser.find_element_by_id(btn_id)
+    cover= browser.find_element_by_class_name(cover_class)
+    click_btn2(btn, cover)
 
 
 def scrap_sh(start_date=None,date_str=""):
@@ -46,11 +72,11 @@ def scrap_sh(start_date=None,date_str=""):
     
     
     if start_date is not None:
-        time.sleep(5)
+        time.sleep(10)
         start=browser.find_element_by_id("start_date2")
         btn=browser.find_element_by_id("btnQuery")
         browser.execute_script("arguments[0].value=arguments[1].toString()",start,start_date)
-        btn.click()
+        click_btn("btnQuery", "sse_search_input_con")
     
     while work:
         time.sleep(5)
@@ -70,7 +96,8 @@ def scrap_sh(start_date=None,date_str=""):
         
     
         but=browser.find_element_by_css_selector(".public_texthre > a")
-        but.click()
+        cov=browser.find_element_by_class_name("sse_search_box")
+        click_btn2(but, cov)
         
         time.sleep(1)
         
@@ -84,8 +111,9 @@ def scrap_sh(start_date=None,date_str=""):
                 col_names.append(col_name.text)
             
             items=[]
-            for row in table.find_elements_by_tag_name("tr")[1::9]:
-                item={"日期":today}
+            ind=0
+            for row in table.find_elements_by_tag_name("tr")[1:]:
+                
 #                 print(row.text)
                 cols=row.find_elements_by_tag_name("td")
                 
@@ -93,13 +121,18 @@ def scrap_sh(start_date=None,date_str=""):
                 if len_col==1:
                     continue
                 if len_col!=len(col_names):
-                    print("bad length")
-                    for c in cols:
-                        print(c.text)
+#                     print("bad length,",len_col)
+# #                     continue
+# #                     for c in cols:
+# #                         print("bad row ",ind,c.text)
+#                     ind+=1
+                    continue
+                item={"日期":today}
                 for i in range(len_col):
                     item[col_names[i]]=cols[i].text
                     
                 item["原因"]=titles[types].text
+                print(item)
                 items.append(item)
                     
             
@@ -115,6 +148,7 @@ def scrap_sh(start_date=None,date_str=""):
                 
                 for i in range(1,len(rows)):
                     row=rows[i]
+                    print("row text",row.text)
                     cols=row.find_elements_by_tag_name("td")
                     if len(cols)==1:
                         continue
@@ -122,35 +156,29 @@ def scrap_sh(start_date=None,date_str=""):
                     
                     mantis={}
                     for j in range(len(cols)):
-                        if len(sub_col_names)==0:
+                        if len(sub_col_names[j])==0:
                             continue
                         mantis[sub_col_names[j]]=cols[j].text
                 
                     mantis_list.append(mantis)
+                    print(mantis)
                     
                 items[ind]["详情"]=mantis_list
+                output(items[ind])
                 ind+=1
                 
-            if len(items)>0:
-                output(items)
+            print(titles[types].text)
+            print("length:",len(items))
+            print(items)
+            print("--------------")
         
         if work:
-            pre_btn=browser.find_element_by_id("preBtn")
-            try:
-                cover= browser.find_element_by_class_name("row mobile_flex")
-                wait = WebDriverWait(browser,60);
-                wait.until(EC.invisibility_of_element(cover));
-                
-                
-                wait.until(EC.element_to_be_clickable(pre_btn ));
-            except:
-                pass
-            pre_btn.click() 
-
+            but.click()
+            click_btn("preBtn","mobile_flex")
         else:
             browser.close()
         
-scrap_sh(start_date="2021-03-23",date_str="2019-01-01")
+scrap_sh(start_date="2019-11-27",date_str="2019-01-01")
 
 
 # def compute_value(csi500,levera):
