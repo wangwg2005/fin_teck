f54ea1f83631ca304136647b2d322393066688cc
Darren <wangwg2005@163.com>
2021-06-29 17:02:22 +0800

    update

--- src/study/leverage/main.py
+++ src/study/leverage/main.py
@@ -10,8 +10,30 @@ pre_day=tutil.get_prevous_trade_date(ttoday)
 
 pre2_day=tutil.get_prevous_trade_date(pre_day)
 
-df1=lreader.read_detail_sse(tutil.stringfy(pre_day))
+pre_day_str=tutil.stringfy(pre_day)
+df1=lreader.read_detail_sse(pre_day_str)
 
-df2=lreader.read_detail_sse(tutil.stringfy(pre2_day))
+pre2_day_str=tutil.stringfy(pre2_day)
+df2=lreader.read_detail_sse(pre2_day_str)
 
-print(df.head())
+
+
+def reindex(df):
+    ind_ser=df.pop("标的证券代码")
+    df.index=ind_ser 
+
+
+
+reindex(df1)
+reindex(df2)
+
+df=df1[["标的证券简称"]]
+df["day2"]=df1["本日融资余额(元)"]
+df["day1"]=df2["本日融资余额(元)"]
+
+df["incr"]=df["day2"]/df["day1"]-1
+
+dfn=df.sort_values(by=['incr'],ascending=False)
+
+print(dfn.head(20))
+
