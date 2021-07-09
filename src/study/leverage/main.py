084100253f9087185c5aff830d8c141b01157dd7
Darren <wangwg2005@163.com>
2021-07-09 09:35:27 +0800

    use picture to present data

--- src/study/leverage/main.py
+++ src/study/leverage/main.py
@@ -3,6 +3,7 @@ import datetime
 from study.leverage import time_util as tutil
 from study.leverage import leverage_reader as lreader
 import pandas as pd
+import matplotlib.pyplot as plt 
 
 ttoday=datetime.date.today()
 
@@ -21,9 +22,11 @@ def analyze_sse():
     
     
     df=df1[["标的证券简称"]]
+    df=df.rename(columns={"标的证券简称":"证券简称"})
     df[pre_day_str]=df1["本日融资余额(元)"]
     df[pre2_day_str]=df2["本日融资余额(元)"]
     
+    df=df.filter(regex="^60", axis=0)
     df["incr"]=df[pre_day_str]/df[pre2_day_str]-1
     df["quant"]=df[pre_day_str]-df[pre2_day_str]
     
@@ -34,6 +37,11 @@ def analyze_sse():
     dfn2=df.sort_values(by=['quant'],ascending=False)
     
     print(dfn2.head(20))
+#     dfn[:20].plot(x="证券简称",y="incr",kind="bar",rot="30")
+#     dfn2[:20].plot(x="证券简称",y="incr",kind="bar",rot="30")
+    
+    return dfn[:20],dfn2[:20]
+    
     
 def convert_number(a):
     return int(a.replace(",",""))
@@ -49,16 +57,33 @@ def analyze_szse():
     df=df1[["证券简称"]]
     df[pre_day_str]=df1["融资余额(元)"].map(convert_number)
     df[pre2_day_str]=df2["融资余额(元)"].map(convert_number)
-    
+    df=df.filter(like="00", axis=0)
     df["incr"]=df[pre_day_str]/df[pre2_day_str]-1
     df["quant"]=df[pre_day_str]-df[pre2_day_str]
     
     dfn=df.sort_values(by=['incr'],ascending=False)
-    
+#     dfn[:20].plot(x="证券简称",y="incr",kind="bar",rot="30")
     print(dfn.head(20))
     
     dfn2=df.sort_values(by=['quant'],ascending=False)
-    
+#     dfn2[:20].plot(x="证券简称",y="incr",kind="bar",rot="30")
     print(dfn2.head(20))
+    
+    return dfn[:20],dfn2[:20]
 
-analyze_szse()
+sse1, sse2=analyze_sse()
+szse1, szse2=analyze_szse()
+
+incr=pd.concat([sse1,szse1])
+ratio = pd.concat([sse2,szse2])
+
+figure,ax=plt.subplots(2,1)
+figure.suptitle(pre_day)
+# ax1=plt.figure(2,1,1)
+incr=incr.sort_values(by=['incr'],ascending=False)
+incr[:20].plot(x="证券简称",y="incr",kind="bar",rot="30",ax=ax[0])
+# ax2=plt.figure(2,1,2)
+ratio=ratio.sort_values(by=['quant'],ascending=False)
+ratio[:20].plot(x="证券简称",y="quant",kind="bar",rot="30",ax=ax[1])
+
+plt.show()
