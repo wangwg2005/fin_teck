c408cefb48aebcab38a843629cbba5338a58c803
Darren <wangwg2005@163.com>
2021-07-23 16:40:19 +0800

    sort by buyin, not the increment

--- src/study/leverage/main.py
+++ src/study/leverage/main.py
@@ -23,6 +23,7 @@ def filter_sse():
     
     
     df=df1[["标的证券简称"]]
+    df["融资买入额(元)"]=df1["本日融资买入额(元)"]
     df=df.rename(columns={"标的证券简称":"证券简称"})
     df[pre_day_str]=df1["本日融资余额(元)"]
     df[pre2_day_str]=df2["本日融资余额(元)"]
@@ -58,6 +59,7 @@ def filter_szse():
     df=df1[["证券简称"]]
     df[pre_day_str]=df1["融资余额(元)"].map(convert_number)
     df[pre2_day_str]=df2["融资余额(元)"].map(convert_number)
+    df["融资买入额(元)"]=df1["融资买入额(元)"].map(convert_number)
     df=df.filter(like="00", axis=0)
     df["incr"]=df[pre_day_str]/df[pre2_day_str]-1
     df["quant"]=df[pre_day_str]-df[pre2_day_str]
@@ -85,8 +87,8 @@ def process():
     incr=incr.sort_values(by=['incr'],ascending=False)
     incr[:20].plot(x="证券简称",y="incr",kind="bar",rot="30",ax=ax[0])
     # ax2=plt.figure(2,1,2)
-    ratio=ratio.sort_values(by=['quant'],ascending=False)
-    ratio[:20].plot(x="证券简称",y="quant",kind="bar",rot="30",ax=ax[1])
+    ratio=ratio.sort_values(by=["融资买入额(元)"],ascending=False)
+    ratio[:20].plot(x="证券简称",y=["quant","融资买入额(元)"],kind="bar",rot="30",ax=ax[1])
     
     
     plt.show()
