68e76ef80bf148a33c9a44e62d30495bb503f905
Darren <wangwg2005@163.com>
2022-09-09 09:25:15 +0800

    daily commit

--- src/javascript/js/analyze.js
+++ src/javascript/js/analyze.js
@@ -18,6 +18,24 @@ function convert_date(data){
 	
 }
 
+function continus_date(data){
+	var inds = [];
+	
+	for(let ind in data){
+		if (date_filter(data[ind])){
+			inds.push(ind);
+			// r1.push(data[ind]);
+			// r2.push(data[ind+1]);
+		}
+	}
+	console.log(inds);
+	var r1 = Array.from(inds, ind => data[ind]);
+	var r2 = Array.from(inds, ind => data[ind+1]);
+	
+	return [r1,r2];
+	
+}
+
 function get_by_week(data){
 	for (d of data){
 		if ("date" in d){
